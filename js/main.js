52668854eddceaecc962f2c4d3f09bbe2600d40f
Alper <alpersarginercun@gmail.com>
2024-01-26 11:58:11 +0300

    basket section: item list-remove

--- js/main.js
+++ js/main.js
@@ -1,4 +1,5 @@
-let productList = [];
+let productList = [],
+  basketList = [];
 
 const toggleModal = () => {
   const basketCloseIcon = document.querySelector(".basket_section");
@@ -29,13 +30,74 @@ const createProductItemsHtml = () => {
        }" class="shop_page_images"></a></div>
        <div class="padding_15">
           <h2 class="product_name_text">${product.name}</h2>
-          <div class="add_bt_1"><a href="#">SEPETE EKLE</a></div>
+          <div class="add_bt_1" onclick="addItemToBasket(${
+            product.id
+          })">SEPETE EKLE</div>
        </div>
     </div>
  </div>`;
   });
 
   productListEl.innerHTML = productListHtml;
+};
+
+const listBasketItems = () => {
+  const basketListEl = document.querySelector(".basket_list");
+  const basketCountEl = document.querySelector(".basket_count");
+  basketCountEl.innerHTML = basketList.length > 0 ? basketList.length : null;
+  const totalPriceEl = document.querySelector(".total_price");
+
+  let basketListHtml = "";
+  let totalPrice = 0;
+
+  basketList.forEach(item => {
+    totalPrice += item.product.price;
+    basketListHtml += `<li class="basket_item">
+    <img src="${item.product.imgSource}" width="100" height="100" alt="">
+    <div class="basket_item_info">
+       <h4 class="product_name">${item.product.name}</h4>
+       <span class="product_price">${item.product.price} ₺</span> <br>
+       <span class="product_remove" onclick="removeItemToBasket(${item.product.id})">Sil</span>
+    </div>
+    <div class="product_count">
+       <span class="decrease">-</span>
+       <span>${item.quantity}</span>
+       <span class="increase">+</span>
+    </div>
+ </li>`;
+  });
+
+  basketListEl.innerHTML = basketListHtml
+    ? basketListHtml
+    : `<li class="basket_item">Sepetinizde ürün bulunmuyor</li>`;
+  totalPriceEl.innerHTML =
+    totalPrice > 0 ? "Sepet Tutarı: " + totalPrice.toFixed(2) + " ₺" : null;
+};
+
+const addItemToBasket = productId => {
+  let foundProduct = productList.find(product => product.id == productId);
+  if (foundProduct) {
+    const basketAlreadyIndex = basketList.findIndex(
+      basket => basket.product.id == productId
+    );
+    if (basketAlreadyIndex == -1) {
+      let addedItem = { quantity: 1, product: foundProduct };
+      basketList.push(addedItem);
+    } else {
+      basketList[basketAlreadyIndex].quantity += 1;
+    }
+    listBasketItems();
+  }
+};
+
+const removeItemToBasket = productId => {
+  const foundIndex = basketList.findIndex(
+    basket => basket.product.id == productId
+  );
+  if (foundIndex != -1) {
+    basketList.splice(foundIndex, 1);
+  }
+  listBasketItems();
 };
 
 setTimeout(() => {
